b77ea073e8266d30c4d31ad1c112412a24ae5dc6
Elad Meidar <elad@shinobidevs.com>
2016-10-06 12:37:45 +0300

    [FEAT]

--- lib/chrono_api.rb
+++ lib/chrono_api.rb
@@ -1,5 +1,25 @@
 require "chrono_api/version"
-
+require 'httparty'
 module ChronoApi
-  # Your code goes here...
+  include HTTParty
+
+  base_uri "https://chrono-api.herokuapp.com/api/v1"
+
+  format :json
+
+  def self.create_timer!
+    self.post("/timers").parsed_response
+  end
+
+  def self.get_timer(uuid)
+    self.get("/timers", query: {uuid: uuid}).parsed_response
+  end
+
+  def self.stop_timer(uuid)
+    self.put("/timers/#{uuid}").parsed_response
+  end
+
+  def self.delete_timer(uuid)
+    self.delete("/timers/#{uuis}", query: {id: uuid}).parsed_response
+  end
 end
